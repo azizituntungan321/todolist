fa5feaf9d40e39a9af876f96a780a83914982973
Azizi Mukmin <azizimukmin16@gmail.com>
2022-10-28 22:26:21 +0700

    config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import ormconfig from './config/orm.config'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(ormconfig),
-    TodoModule
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: `${process.env.MYSQL_USER}`,
+      password: '',
+      database: 'azi',
+      entities: [],
+      synchronize: true,
+    }),
+    TodoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
